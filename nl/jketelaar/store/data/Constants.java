deb58ec462793fe2d360de25bc3c8c4dd712b04a
Jeroen Ketelaar <JKetelaar@users.noreply.github.com>
2015-04-14 16:09:32 +0200

    Create Constants.java

---
+++ nl/jketelaar/store/data/Constants.java
@@ -0,0 +1,9 @@
+package nl.jketelaar.store.data;
+
+/**
+ * @author JKetelaar
+ */
+public class Constants {
+    public static final int STORE_ID = 3824;
+    public static final int SEARCH_ID = 41409;
+}
